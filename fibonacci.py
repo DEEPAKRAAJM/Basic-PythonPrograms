38974962c09b469da2356a380ce6417771c43099
DEEPAKRAAJM <33368883+DEEPAKRAAJM@users.noreply.github.com>
2018-05-12 17:31:01 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,10 @@
 ###### Program for Calculating the Fibanacci Series #######
 
-num= input("Enter the NO: ")
+num= int(input("Enter the NO: "))
 n=num
-t1, t2, t3=0
+t1, t2, t3=0,0,0
 for i in range(1, n):
-	print t1
+	print (t1)
 	t3=t1+t2
+	t1=t2
 	t2=t3
